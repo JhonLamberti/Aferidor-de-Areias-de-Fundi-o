52ef45c7bb1621887e3d902dc6653fee4f8a1210
JhonLamberti <jotalamberti@gmail.com>
2022-11-02 20:15:09 -0300

    hexa

---
+++ app.js
@@ -0,0 +1,41 @@
+const Areia = document.getElementById("#Areia")
+const pcResina = document.querySelector("#porcentagemdeRESINA")
+const pcCatalisador = document.querySelector("#porcentagemdeCATALISADOR")
+
+
+// pcResina.disabled=true
+// pcCatalisador.disabled=true
+console.log(pcResina,pcCatalisador)
+console.log(Areia)
+
+
+let valordaAreia = null
+
+
+const saveareia = (num) => {
+    valordaAreia = parseInt(num)
+    console.log(valordaAreia)
+  }
+
+
+
+
+
+
+  const salvardados = () => {
+    
+    
+
+    const vlrAreia = parseInt(document.getElementById("valorareia").value)
+    console.log(areia)
+    console.log(porcentagemR)
+    console.log(porcentagemC)
+    console.log(vlrAreia)
+  }
+
+  const aparecer = () => {
+  
+
+
+
+  }
