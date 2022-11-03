dd3b99231e6712da16ce65e5972cbe1ab0586af6
RobertCardoso <robert.rcardoso@hotmail.com>
2022-11-02 19:47:58 -0500

    select funcionando

--- app.js
+++ app.js
@@ -1,41 +1,78 @@
-const Areia = document.getElementById("#Areia")
-const pcResina = document.querySelector("#porcentagemdeRESINA")
-const pcCatalisador = document.querySelector("#porcentagemdeCATALISADOR")
 
+const areia = document.querySelector("#Areia")
+const pcResina = document.querySelector("#pRESINA")
+const pcCatalisador = document.querySelector("#pCATALISADOR")
 
-// pcResina.disabled=true
-// pcCatalisador.disabled=true
-console.log(pcResina,pcCatalisador)
-console.log(Areia)
-
+pcResina.disabled=true
+pcCatalisador.disabled=true
 
 let valordaAreia = null
-
 
 const saveareia = (num) => {
     valordaAreia = parseInt(num)
     console.log(valordaAreia)
-  }
+}
 
+const aparecer = () => {
+  let resinas = document.getElementById("pRESINA")
+  let catalizadores = document.getElementById("pCATALISADOR")
 
+  resinas.innerHTML = "--"
+  catalizadores.innerHTML = "--"
 
-
-
-
-  const salvardados = () => {
-    
-    
-
-    const vlrAreia = parseInt(document.getElementById("valorareia").value)
-    console.log(areia)
-    console.log(porcentagemR)
-    console.log(porcentagemC)
-    console.log(vlrAreia)
-  }
-
-  const aparecer = () => {
   
+  if(areia.value == "areia de silica"){
+    
+    pcResina.disabled=false
+    pcCatalisador.disabled=false
 
+    resinaSilica = ["","0.8","0.9","1.0","1.1","1.2","1.3","1.4","1.5"];
+    catalisadorSilica =["","20","25","30","35","40","45","50"]
 
+    resinaSilica.forEach((e,k) => {
+      pcResina[k] = new Option(e,k)
+    })
+    catalisadorSilica.forEach((e,k) => {
+      pcCatalisador[k] = new Option(e,k)
+    })
+  }
+  else if(areia.value == "areia de cromita"){
+  
+    pcResina.disabled=false
+    pcCatalisador.disabled=false
+
+    resinaSilica = ["","2","2.1","2.2","2.3","2.4","2.5","2.6","2.7"];
+    catalisadorSilica =["","20","25","30","35","40","45","50"]
+
+    resinaSilica.forEach((e,k) => {
+      pcResina[k] = new Option(e,k)
+    })
+    catalisadorSilica.forEach((e,k) => {
+      pcCatalisador[k] = new Option(e,k)
+    })
+  }
+
+  else if(areia.value == "areia de zirconita"){
+  
+    pcResina.disabled=false
+    pcCatalisador.disabled=false
+
+    resinaSilica = ["","0.8","0.9","1.0","1.1","1.2"];
+    catalisadorSilica =["","15","20","25"]
+
+    resinaSilica.forEach((e,k) => {
+      pcResina[k] = new Option(e,k)
+    })
+    catalisadorSilica.forEach((e,k) => {
+      pcCatalisador[k] = new Option(e,k)
+    })
+  }
+
+  else {
+    pcResina.disabled=true
+    pcCatalisador.disabled=true
 
   }
+
+  
+}
